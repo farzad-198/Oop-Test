33523dac4f9b7c4c9f754ea2f6c4c7e59694c6b9
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-02-22 17:12:12 +0100

    Add Class Person

---
+++ Oop Test/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop_Test
+{
+    internal abstract class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int Age  { get; set; }
+        public int Birthday { get; set; }
+        public string City { get; set; }
+
+        internal abstract void Salery();
+        
+        internal virtual void PrintInfo()
+        {
+            Console.WriteLine($"Name :{Name}\t Family :{Family}\t Age :{Age}\t Birthday :{Birthday}\t City :{City}");
+        
+        }
+    }
+}
